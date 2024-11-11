2036bf0719988333c45fc1d414a712c8770749d8
TROLLKOPF <max.serratosa@gmail.com>
2024-11-11 16:14:28 +0100

    improving code

--- users/areausuarios.php
+++ users/areausuarios.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include_once('db/db.php');
 ?>
 
@@ -6,69 +7,65 @@ include_once('db/db.php');
 <html lang="es">
 
 <head>
-    <!--TÍTULO-->
+    <!-- TÍTULO E ICONO -->
     <title>PAX | DIGITAL MINDS</title>
-    <!--ICONO-->
     <link rel="icon" type="favicon/x-icon" href="../images/icon.png" />
-    <!--LLAMADA A LAS HOJAS DE ESTILO-->
+
+    <!-- LLAMADA A LAS HOJAS DE ESTILO -->
     <link rel="stylesheet" type="text/css" href="../stylesheets/General.css" />
     <link rel="stylesheet" type="text/css" href="../stylesheets/areausuarios.css" />
     <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
 
-    <!--LLAMADA A LOS SCRIPTS-->
-    <script src="../scripts/rrss.js"></script>
+    <!-- LLAMADA A LOS SCRIPTS -->
     <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
     <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
-    <script type="text/javascript" src="../scripts/datepicker.js"></script>
+    <script src="../scripts/rrss.js"></script>
+    <script src="../scripts/datepicker.js"></script>
 
-    <!--METADATOS-->
+    <!-- METADATOS -->
     <?php include('partials/meta.html'); ?>
-
 </head>
 
 <body>
-    <!--INICIO DE LA CABECERA-->
+    <!-- CABECERA -->
     <?php include('partials/header.html'); ?>
 
-    <!--INICIO DE SECCION: CARGAMOS EL AREA DE USUARIO O EL LOGIN SEGÚN SE HAYA INCIADO SESION O NO-->
+    <!-- SECCIÓN PRINCIPAL -->
     <section>
+        <?php
+        if (!isset($_SESSION["valido"]) || $_SESSION["valido"] !== "SI") {
+            include("login/login.php");
+        } else {
+            switch ($_SESSION['rol']) {
+                case 'ADMIN':
+                    include('partials/panelsuperior.php');
+                    include('admin/admin-site.php');
+                    break;
 
-        <section>
-            <?php
-            session_start();
-            if (@$_SESSION["valido"] != "SI") {
-                include("login/login.php");
-            } else {
+                case 'USER':
+                    include('partials/panelsuperior.php');
+                    include('client/client-site.php');
+                    break;
 
-                switch ($_SESSION['rol']) {
-                    case 'ADMIN':
-                        include('partials/panelsuperior.php');
-                        include('admin/admin-site.php');
-                        echo "</div></section><br/>";
-                        break;
-                    case 'USER':
-                        include('partials/panelsuperior.php');
-                        include('client/client-site.php');
-                        echo "</div></section><br/>";
-                        break;
-
-                    default:
-                        include('cerrarSesion.php');
-                        echo "<script>
-                        $(document).ready(function({
-                            $('errorMsg').html('<span class=`nvaliduser`>Error en el proceso, contacta con el administrador</span>');
+                default:
+                    // Destruir la sesión y mostrar error en caso de rol desconocido
+                    session_unset();
+                    session_destroy();
+                    echo "<script>
+                        $(document).ready(function() {
+                            $('#errorMsg').html('<span class=\"invaliduser\">Error en el proceso, contacta con el administrador</span>');
                         });
-                        </script>
-                        ";
-
-                        break;
-                }
+                    </script>";
+                    break;
             }
-            ?>
-        </section>
+        }
+        ?>
     </section>
+
+    <!-- MENSAJE DE ERROR -->
     <div id="errorMsg"></div>
-    <!--PIE DE PÁGINA-->
+
+    <!-- PIE DE PÁGINA -->
     <?php include('partials/footer.html'); ?>
 
 </body>
